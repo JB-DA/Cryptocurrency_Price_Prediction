f4e135ecf693696265f6dd6208ddead9cf7e3c16
JB-DA <70473223+JB-DA@users.noreply.github.com>
2021-02-21 20:12:02 +0800

    database load

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 
 #from flask import Flask, jsonify, render_template, url_for, redirect
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 import sqlite3
 from sqlalchemy import create_engine
 import pandas as pd
@@ -23,8 +23,11 @@ app = Flask(__name__, template_folder='static', static_folder='static')
 # API Pages
 @app.route("/api/assets")
 def api_overview():
+
+    asset = request.args.get('asset')
+
     con = sqlite3.connect("crypto_db.sqlite")
-    df = pd.read_sql_query("SELECT * from historic_trades WHERE asset_id='brentoil'", con)
+    df = pd.read_sql_query(f"SELECT * from historic_trades WHERE asset_id='{asset}'", con)
     json_assets = json.loads(df.to_json(orient='records'))
     #df_csv.to_sql("historic_trades", con, if_exists="replace")
     con.close()
@@ -42,4 +45,7 @@ def index():
 ###
 #
 if __name__ == '__main__':
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
     app.run(debug=True)
+    
--- static/js/test.js
+++ static/js/test.js
@@ -1,78 +1,78 @@
 alert("The Index Javascript file was loaded...");
 
-// var coinlist = {};
-// let chartURL = "/api/assets";
+var coinlist = {};
+let chartURL = "/api/assets";
 
-// function init() { //default graph to Bitcoin
+function init() { //default graph to Bitcoin
 
-//     var url = chartURL;
-//     console.log(url);
+    var url = chartURL;
+    console.log(url);
 
-//     d3.json(url).then(function (data) {
+    d3.json(url).then(function (data) {
 
-//         var startDate = [];
-//         var endDate = [];
-//         var dates = [];
-//         var openingPrices = [];
-//         var highPrices = [];
-//         var lowPrices = [];
-//         var closingPrices = [];
-//         var stock = "brentoil"
+        var startDate = [];
+        var endDate = [];
+        var dates = [];
+        var openingPrices = [];
+        var highPrices = [];
+        var lowPrices = [];
+        var closingPrices = [];
+        var stock = "brentoil"
 
-//         data.forEach(function (d) {
-//             startDate.push(d.time_period_start);
-//             endDate.push(d.time_period_end);
-//             dates.push(d.time_open);
-//             openingPrices.push(d.price_open);
-//             highPrices.push(d.price_high);
-//             lowPrices.push(d.price_low);
-//             closingPrices.push(d.price_close);
-//         });
+        data.forEach(function (d) {
+            startDate.push(d.time_period_start);
+            endDate.push(d.time_period_end);
+            dates.push(d.time_open);
+            openingPrices.push(d.price_open);
+            highPrices.push(d.price_high);
+            lowPrices.push(d.price_low);
+            closingPrices.push(d.price_close);
+        });
 
-//         var trace1 = {
-//             type: "scatter",
-//             mode: "lines",
-//             //name: name,
-//             x: dates,
-//             y: closingPrices,
-//             line: {
-//                 color: "#063970"
-//             }
-//         };
+        var trace1 = {
+            type: "scatter",
+            mode: "lines",
+            //name: name,
+            x: endDate,
+            y: closingPrices,
+            line: {
+                color: "#063970"
+            }
+        };
 
-//         // Candlestick Trace
-//         var trace2 = {
-//             type: "candlestick",
-//             x: dates,
-//             high: highPrices,
-//             low: lowPrices,
-//             open: openingPrices,
-//             close: closingPrices
-//         };
+        // Candlestick Trace
+        var trace2 = {
+            type: "candlestick",
+            x: endDate,
+            high: highPrices,
+            low: lowPrices,
+            open: openingPrices,
+            close: closingPrices
+        };
 
-//         var data = [trace1, trace2];
-//         //console.log(data);
+        var data = [trace1, trace2];
+        //console.log(data);
 
-//         var layout = {
-//             title: `${stock} closing prices in $USD`,
-//             font: {
-//                 family: 'ABeeZee',
-//                 size: 12,
-//                 color: '#7f7f7f'
-//               },
-//             xaxis: {
-//                 range: [startDate, endDate],
-//                 type: "date"
-//             },
-//             yaxis: {
-//                 autorange: true,
-//                 type: "linear"
-//             },
-//             showlegend: false
-//         };
+        var layout = {
+            title: `${stock} closing prices in $USD`,
+            font: {
+                family: 'ABeeZee',
+                size: 12,
+                color: '#7f7f7f'
+              },
+            xaxis: {
+                range: [startDate, endDate],
+                type: "date"
+            },
+            yaxis: {
+                autorange: true,
+                type: "linear"
+            },
+            showlegend: false
+        };
 
-//         Plotly.newPlot("mainchartdiv", data, layout);
-//     });
-// } //END DEFAULT PLOT
+        Plotly.newPlot("mainchartdiv", data, layout);
+    });
+} //END DEFAULT PLOT
 
-// init();
+init();
