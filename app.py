5d940674b9ae6a040ad08b3aba70cbff8801306b
JB-DA <70473223+JB-DA@users.noreply.github.com>
2021-02-27 16:18:53 +0800

    test

--- app.py
+++ app.py
@@ -6,20 +6,42 @@ from sqlalchemy import create_engine
 import pandas as pd
 import json
 
+
+##### DATABASE SETUP
+###
+#
+engine = create_engine(f"sqlite:///crypto_db.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
 app = Flask(__name__, template_folder='static', static_folder='static')
 
+
 # ROUTES
+# API Pages
+@app.route("/api/asset")
+def api_asset():
+    asset = request.args.get('asset')
+
+    dbConnect = engine.connect()
+    query = f"SELECT time_period_end, price_close from historic_data WHERE asset_id='{asset}'"
+    df = pd.read_sql(query , conn)
+
+    json_overview = json.loads(df.to_json(orient='records'))
+    dbConnect.close()
+    return jsonify(json_overview)
+
 @app.route("/api/assets")
 def api_assets():
-
     #asset = request.args.get('asset')
 
-    con = sqlite3.connect("crypto_db.sqlite")
-    df = pd.read_sql_query(f"SELECT DISTINCT asset_id FROM historic_data", con)
-    json_assets = json.loads(df.to_json(orient='records'))
-    #df_csv.to_sql("historic_trades", con, if_exists="replace")
-    con.close()
-    return jsonify(json_assets)
+    dbConnect = engine.connect()
+    query = f"SELECT DISTINCT asset_id FROM historic_data"
+    df = pd.read_sql(query , conn).head(29)
+
+    json_overview = json.loads(df.to_json(orient='records'))
+    dbConnect.close()
+    return jsonify(json_overview)
 
 # HTML PAGES
 @app.route('/')
